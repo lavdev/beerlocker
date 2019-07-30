468e1639be86c1abb534d609366a52e00c82e0b2
Laverson Espindola <laverson.espindola@gmail.com>
2019-07-30 17:01:14 -0300

    "Styleish" dialog to allow/deny client

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,19 +7,29 @@ var Client = require('../models/client');
 var Token = require('../models/token');
 
 passport.use(new BasicStrategy(
-  function(username, password, callback) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return callback(err); }
+  function (username, password, callback) {
+    User.findOne({
+      username: username
+    }, function (err, user) {
+      if (err) {
+        return callback(err);
+      }
 
       // No user found with that username
-      if (!user) { return callback(null, false); }
+      if (!user) {
+        return callback(null, false);
+      }
 
       // Make sure the password is correct
-      user.verifyPassword(password, function(err, isMatch) {
-        if (err) { return callback(err); }
+      user.verifyPassword(password, function (err, isMatch) {
+        if (err) {
+          return callback(err);
+        }
 
         // Password did not match
-        if (!isMatch) { return callback(null, false); }
+        if (!isMatch) {
+          return callback(null, false);
+        }
 
         // Success
         return callback(null, user);
@@ -29,12 +39,18 @@ passport.use(new BasicStrategy(
 ));
 
 passport.use('client-basic', new BasicStrategy(
-  function(username, password, callback) {
-    Client.findOne({ id: username }, function (err, client) {
-      if (err) { return callback(err); }
+  function (username, password, callback) {
+    Client.findOne({
+      id: username
+    }, function (err, client) {
+      if (err) {
+        return callback(err);
+      }
 
       // No client found with that id or bad password
-      if (!client || client.secret !== password) { return callback(null, false); }
+      if (!client || client.secret !== password) {
+        return callback(null, false);
+      }
 
       // Success
       return callback(null, client);
@@ -43,26 +59,46 @@ passport.use('client-basic', new BasicStrategy(
 ));
 
 passport.use(new BearerStrategy(
-  function(accessToken, callback) {
-    Token.findOne({value: accessToken }, function (err, token) {
-      if (err) { return callback(err); }
+  function (accessToken, callback) {
+    Token.findOne({
+      value: accessToken
+    }, function (err, token) {
+      if (err) {
+        return callback(err);
+      }
 
       // No token found
-      if (!token) { return callback(null, false); }
+      if (!token) {
+        return callback(null, false);
+      }
 
-      User.findOne({ _id: token.userId }, function (err, user) {
-        if (err) { return callback(err); }
+      User.findOne({
+        _id: token.userId
+      }, function (err, user) {
+        if (err) {
+          return callback(err);
+        }
 
         // No user found
-        if (!user) { return callback(null, false); }
+        if (!user) {
+          return callback(null, false);
+        }
 
         // Simple example with no scope
-        callback(null, user, { scope: '*' });
+        callback(null, user, {
+          scope: '*'
+        });
       });
     });
   }
 ));
 
-exports.isAuthenticated = passport.authenticate(['basic', 'bearer'], { session : false });
-exports.isClientAuthenticated = passport.authenticate('client-basic', { session : false });
-exports.isBearerAuthenticated = passport.authenticate('bearer', { session: false });
+exports.isAuthenticated = passport.authenticate(['basic', 'bearer'], {
+  session: false
+});
+exports.isClientAuthenticated = passport.authenticate('client-basic', {
+  session: false
+});
+exports.isBearerAuthenticated = passport.authenticate('bearer', {
+  session: false
+});
